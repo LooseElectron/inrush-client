fae62824f35765fcc048920a7deb34c8f82f7cb7
Nikolas Shashok <nikolas.shashok@redlinetrading.com>
2017-08-11 14:29:00 -0400

    Improved table; fixed security info GET

--- rest.js
+++ rest.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
+    var field_id;
+
     $.get("http://10.148.17.158:8080/api/securityinfo", function( data ) {
         for (var i = 0; i < data.length; i++) {
-            $("#datatable").append("<tr class='entry'>" +
-                                "<td class='name'>" + data[i].name + "</td>" +
-                                "<td class='description'>" + data[i].description + "</td>" +
-                              "</tr>" );
+            $("#fieldtable").append("<tr class='entry'>" +
+                                     "<td class='name'>" + data[i].name + "</td>" +
+                                     "<td class='description'>" + data[i].description + "</td>" +
+                                    "</tr>" );
         }
+    });
+
+    $( "#fieldtable" ).on("click", "tr", function() {
+        $(this).addClass('label-info').siblings().removeClass('label-info');
+        field_id = $(this).find('td:first').html();
     });
 
     $( "#rest" ).click(function() {
         var symbol = $( "#symbolInput" ).val();
-            $.get("http://10.148.17.158:8080/api/securities?symbol=" + symbol, function( data ) {
+            $.get("http://10.148.17.158:8080/api/securityinfo?symbol=" + symbol + "&field=" + field_id, function( data ) {
+                console.log( data );
 
-                $("#datatable").html("<tr class='entry'>" +
-                                  "<td class='time'>" + data[0].time + "</td>" +
-                                  "<td class='symbol'>" + data[0].update.symbol + "</td>" +
-                                  "<td class='ask'>" + data[0].update.ask + "</td>" +
-                                  "<td class='bid'>" + data[0].update.bid + "</td>" +
-                                "</tr>" );
-                console.log( { "symbol": data[0].update.symbol,
-                               "ask_price": data[0].update.ask,
-                               "bid_price": data[0].update.bid,
-                               "time": data[0].time } );
+                $("#data").html( data[field_id][0] );
           });
      });
 });
